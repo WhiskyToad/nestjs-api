a066c8e7bfe887909d2f4a0a3ac7847d258235b7
Steven Craig <stevencraig321@gmail.com>
2023-08-17 20:23:56 +0100

    feat: prisma module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, TaskModule],
+  imports: [AuthModule, UserModule, TaskModule, PrismaModule],
   controllers: [],
   providers: [],
 })
