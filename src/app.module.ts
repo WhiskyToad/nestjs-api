7f933e5c0dc96536f86bb8fbf37d15fdaec0c625
Steven Craig <stevencraig321@gmail.com>
2023-08-18 19:14:23 +0100

    nest config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, TaskModule, PrismaModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    TaskModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
